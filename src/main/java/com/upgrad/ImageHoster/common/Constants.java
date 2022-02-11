bed716740273417962fef63d09ac184dab6ed2f7
KUMAR SHUBHAM <krshubham137@gmail.com>
2022-02-11 09:07:50 +0530

    My First Commit

--- src/main/java/com/upgrad/ImageHoster/common/Constants.java
+++ src/main/java/com/upgrad/ImageHoster/common/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
 
     public static final String POST_DIR_NAME="src/main/resources/posts/";
     public static final String POST_FILE_PREFIX="src/main/resources/posts/post";
+public static final String POST_FILE_postfix="src/main/resources/posts/post";
 }
